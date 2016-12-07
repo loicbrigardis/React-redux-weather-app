fdc98bc62b030d0544c2901798df1d7ca477902d
loic <loic.brigardis@gmail.com>
2016-12-07 15:19:14 +0100

    Promises Middleware

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+
+import axios from 'axios';
+
+export const ADD_CARD = 'ADD_CARD';
+
+const API_KEY = '2adf3e90362702fba1dbdfe4b7766d12';
+const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
+// http://api.openweathermap.org/data/2.5/forecast?q=London&appid=?id=524901&appid=${API_KEY}
+
+export function fetchCards () {
+  return {
+    type: ADD_CARD,
+    payload: request
+  }
+}
