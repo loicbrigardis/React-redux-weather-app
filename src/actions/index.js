c5045964760bea580790896369e85aa52844eca0
loic <loic.brigardis@gmail.com>
2016-12-08 11:19:07 +0100

    AddCard ACT + Bind EnterKeyDown

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,9 @@ const API_KEY = '2adf3e90362702fba1dbdfe4b7766d12';
 const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
 // http://api.openweathermap.org/data/2.5/forecast?q=London&appid=?id=524901&appid=${API_KEY}
 
-export function fetchCards () {
+export function addCard (city) {
+  const request = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+
   return {
     type: ADD_CARD,
     payload: request
--- src/containers/weatherContainer.js
+++ src/containers/weatherContainer.js
@@ -1,19 +1,41 @@
 require ('./weatherContainer.scss');
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addCard } from '../actions/index'
 
 import CardClear from './cards/cardClear';
 import CardRain from './cards/CardRain';
 
 class WeatherContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' };
+  }
+
+  handleKeyPress(target) {
+    if(target.charCode==13){
+            alert('Enter clicked!!!');
+    }
+  }
+
   render () {
     return (
-      <ul className="cards">
-        <CardClear />
-        <CardRain />
-      </ul>
+      <div>
+        <div className="inputs">
+          <label className="input_icon">
+            <input  type="text" maxLength="20" onKeyPress={this.handleKeyPress} />
+          </label>
+        </div>
+
+        <ul className="cards">
+          <CardClear />
+          <CardRain />
+        </ul>
+      </div>
     )
   }
 }
 
-export default WeatherContainer;
+export default connect(null, { addCard }) (WeatherContainer);
